abc5e0639bf3ff6101cb19e0dab3e0b5712677a4
Marina <marinaandjelkovic456@gmail.com>
2019-03-18 20:33:38 +0100

    dodat poseban servis za axios, http - skraceni linkovi

--- src/services/PostsService.js
+++ src/services/PostsService.js
@@ -2,11 +2,11 @@ import { httpService } from "./HttpService";
 
 class PostsService {
   createPosts(post) {
-    return httpService.post("https://jsonplaceholder.typicode.com/posts", post);
+    return httpService.post("/posts", post);
   }
 
   getPosts() {
-    return httpService.get("https://jsonplaceholder.typicode.com/posts");
+    return httpService.get("/posts");
   }
 }
 
