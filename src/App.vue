b4a9959aed42ea8fd56100ada770eddaf933d708
Marina <marinaandjelkovic456@gmail.com>
2019-03-18 19:51:53 +0100

    axios, ubacivanje postova preko servisa

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Posts with Axios</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { postsService } from "./services/PostsService";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  components: {},
+  created() {
+    postsService
+      .getPosts()
+      .then(response => response.data)
+      // .then(response => (this.posts = response.data))
+      .then(data => {
+        this.posts = data;
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/services/PostsService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+class PostsService {
+  getPosts() {
+    return axios.get("https://jsonplaceholder.typicode.com/posts");
+  }
+}
+
+export const postsService = new PostsService();
